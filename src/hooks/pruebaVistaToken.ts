1c8a674bd3cc306a029601fbc363f5abb98f3134
AlanGarcia25 <practicanteti@agrocir.com>
2026-02-03 11:08:53 -0700

    Update pruebaVistaToken.ts
    Eliminacion del WebToken

--- src/hooks/pruebaVistaToken.ts
+++ src/hooks/pruebaVistaToken.ts
@@ -7,7 +7,7 @@ export interface TokenInfo {
 }
 
 export const useToken = () => {
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+  const token = "__TOKEN__";
   
   try {
     const decoded = jwtDecode<TokenInfo>(token);
